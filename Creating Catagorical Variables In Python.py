0265f3b3a479f0f9ca18c9a73961d917478c57f4
test-tlb <96823591+test-tlb@users.noreply.github.com>
2022-01-15 23:35:42 +0530

    Add files via upload

---
+++ Creating Catagorical Variables In Python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+s = pd.Series(["a", "b", "c", "a"], dtype="category")
+print(s)
+
+
+# In[7]:
+
+
+import pandas as pd
+car_color = pd.Series(["Blue", "Red", "White", "Merune"], dtype="category")
+print(car_color)
+
+
+# In[ ]:
+
+
+import pandas as pd
+
